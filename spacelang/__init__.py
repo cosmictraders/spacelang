fa60a47dfd79c0b04be95046e883c22f1b0f32e2
Ashwin Naren <arihant2math@gmail.com>
2023-05-24 21:04:47 -0700

    moved step class to its own file

--- spacelang/__init__.py
+++ spacelang/__init__.py
@@ -1,119 +1,12 @@
-import math
 import time
-from datetime import datetime, timezone
 from threading import Thread
 
 import yaml
 from autotraders import session as s
-from autotraders.map.waypoint import Waypoint
 from autotraders.ship import Ship
 
 import secret
-
-
-def load_text(stream):
-    data = yaml.load(stream, Loader=yaml.Loader)
-    return data
-
-
-class Step:
-    def __init__(self, command, data):
-        self.command = command
-        self.args = data
-
-    def navigate(self, ship, session):
-        if type(self.args["destination"]) is str:
-            ship.navigate(self.command)
-        else:
-            choices = Waypoint.all(ship.nav.location.system, session)
-            ship_current_waypoint = [choice for choice in choices if choice.symbol == ship.nav.location][0]
-            tmp = []
-            for choice in choices:
-                if "trait" in self.args["destination"]:
-                    if len([trait for trait in choice.traits if
-                            trait.symbol == self.args["destination"]["trait"]]) != 0:
-                        tmp.append(choice)
-                if "type" in self.args["destination"]:
-                    if choice.waypoint_type == self.args["destination"]["type"]:
-                        tmp.append(choice)
-            choices = tmp
-            if len(choices) == 0:
-                raise Exception("0 possible waypoints")
-            if "selection" not in self.args["destination"] or self.args["destination"][
-                "selection"] == "NEAREST":
-                accepted = choices[0]
-                best = 1000000
-                for choice in choices:
-                    if choice.symbol == ship.nav.location:
-                        accepted = choice
-                        best = -1
-                    distance = math.sqrt(
-                        ((choice.x - ship_current_waypoint.x) ** 2) + ((choice.y - ship_current_waypoint.y) ** 2))
-                    if distance < best:
-                        best = distance
-                        accepted = choice
-            else:
-                accepted = choices[0]
-            if str(accepted.symbol) != str(ship.nav.location):
-                ship.navigate(str(accepted.symbol))
-            time.sleep(1)
-            ship.update()
-            if ship.nav.status == "IN_TRANSIT":
-                time.sleep((ship.nav.route.arrival - datetime.now(timezone.utc)).seconds)
-
-    def action(self, ship, events, session):
-        repeat_interval = -1
-        if type(self.args) is str:
-            name = self.args
-            repeat = False
-        else:
-            name = self.args["name"]
-            repeat = "repeat" in self.args
-            if repeat and "interval" in self.args["repeat"]:
-                repeat_interval = self.args["repeat"]["interval"]
-        for event in events:
-            if event.name == name:
-                if repeat:
-                    if repeat_interval != -1:
-                        while True:
-                            event.execute(ship, events, session)
-                            time.sleep(repeat_interval)
-                else:
-                    event.execute(ship, events, session)
-
-    def execute(self, ship: Ship, events, session):
-        if self.command == "navigate":
-            self.navigate(ship, session)
-        elif self.command == "action":
-            self.action(ship, events, session)
-        elif self.command == "orbit":
-            ship.orbit()
-        elif self.command == "dock":
-            ship.dock()
-        elif self.command == "refuel":
-            ship.refuel()
-        elif self.command == "extract":
-            ship.extract()
-        elif self.command == "refine":
-            ship.refine(self.args)
-        elif self.command == "sell":
-            assert type(self.args) is dict
-            ship.sell(self.args["symbol"], self.args.get("quantity", 1))
-        elif self.command == "buy":
-            assert type(self.args) is dict
-            ship.buy(self.args["symbol"], self.args.get("quantity", 1))
-        elif self.command == "sellall":
-            for i in ship.cargo.inventory:
-                ship.sell(i, ship.cargo.inventory[i])
-                time.sleep(0.1)
-        elif self.command == "sleep":
-            if type(self.args) is int:
-                time.sleep(self.args)
-            else:
-                raise NotImplementedError("TODO")
-        else:
-            raise NotImplementedError("TODO")
-        time.sleep(1)
+from spacelang.step import Step
 
 
 class Action:
@@ -159,7 +52,7 @@ class File:
         self.triggers = [Trigger(d, data["triggers"][d]) for d in data["triggers"]]
 
     def run(self, session):
-        print("[main] Initializing ship state ...")
+        print("[main] Initializing ship state ...")  # TODO: Tech Debt on threads (use logging instead)
         ships = {}
         for group in self.ship_groups:
             ships[group] = [Ship(ship, session) for ship in self.ship_groups[group]]
@@ -173,6 +66,11 @@ class File:
         while True:
             print("[main] Checking triggers ...")
             time.sleep(5)
+
+
+def load_text(stream):
+    data = yaml.load(stream, Loader=yaml.Loader)
+    return data
 
 
 if __name__ == "__main__":
